78dc22dbb4a38d9f5052d327922230fa3e48588c
Caleb Likens <clikens34@gmail.com>
2021-06-26 11:44:36 -0400

    fixed a bit of styles

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -56,7 +56,8 @@ export const RecipeTemplate = ({
               <ul className="recipe__list recipe__ingredients">
                 {ingredients && ingredients.map((ing, idx) => ( 
                   <li className="recipe__list-item recipe__ingredients-item" key={idx}>
-                    <input className="recipe__list-check" type="checkbox" id={`i${idx}`} />
+                    <input className="recipe__list-input" type="checkbox" id={`i${idx}`} />
+                    <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="recipe__list-check" width="15" height="15"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
 										<label className="recipe__list-label" htmlFor={`i${idx}`}>
 											{ing.quantity ? `${ing.quantity} ` : ``}
 											{ing.measurement ? `${ing.measurement}(s) ` : ``}
@@ -73,7 +74,8 @@ export const RecipeTemplate = ({
               <ol className="recipe__list recipe__directions">
                 {directions && directions.map((dir, idx) => ( 
                   <li className="recipe__list-item recipe__directions-item" key={idx}>
-                      <input className="recipe__list-check" type="checkbox" id={`d${idx}`} />
+                      <input className="recipe__list-input" type="checkbox" id={`d${idx}`} />
+                      <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="recipe__list-check" width="15" height="15"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
 											<label className="recipe__list-label" htmlFor={`d${idx}`}>
 												{dir.direction}
 											</label>
